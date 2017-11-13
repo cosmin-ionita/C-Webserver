bbebaeeeb2a505fc5cb11e5df2861fb12a1cbc81
Ionita Cosmin <ionita.cosmin@outlook.com>
2017-11-13 18:56:05 +0200

    Refactored project files

--- Webserver/server.c
+++ Webserver/server.c
@@ -19,22 +19,22 @@ struct {
 	char *ext;
 	char *filetype;
 } extensions [] = {
-	{"gif", "image/gif" },  
-	{"jpg", "image/jpeg"}, 
+	{"gif", "image/gif" },
+	{"jpg", "image/jpeg"},
 	{"jpeg","image/jpeg"},
-	{"png", "image/png" },  
-	{"zip", "image/zip" },  
-	{"gz",  "image/gz"  },  
-	{"tar", "image/tar" },  
-	{"htm", "text/html" },  
-	{"html","text/html" },  
-	{"php", "image/php" },  
-	{"cgi", "text/cgi"  },  
-	{"asp","text/asp"   },  
-	{"jsp", "image/jsp" },  
-	{"xml", "text/xml"  },  
+	{"png", "image/png" },
+	{"zip", "image/zip" },
+	{"gz",  "image/gz"  },
+	{"tar", "image/tar" },
+	{"htm", "text/html" },
+	{"html","text/html" },
+	{"php", "image/php" },
+	{"cgi", "text/cgi"  },
+	{"asp","text/asp"   },
+	{"jsp", "image/jsp" },
+	{"xml", "text/xml"  },
 	{"js","text/js"     },
-   {"css","test/css"   }, 
+   {"css","test/css"   },
 
 	{0,0} };
 
@@ -45,17 +45,17 @@ void log(int type, char *s1, char *s2, int num)
 
 	switch (type) {
 	case ERROR: (void)sprintf(logbuffer,"ERROR: %s:%s Errno=%d exiting pid=%d",s1, s2, errno,getpid()); break;
-	case SORRY: 
+	case SORRY:
 		(void)sprintf(logbuffer, "<HTML><BODY><H1>Web Server Sorry: %s %s</H1></BODY></HTML>\r\n", s1, s2);
 		(void)write(num,logbuffer,strlen(logbuffer));
-		(void)sprintf(logbuffer,"SORRY: %s:%s",s1, s2); 
+		(void)sprintf(logbuffer,"SORRY: %s:%s",s1, s2);
 		break;
 	case LOG: (void)sprintf(logbuffer," INFO: %s:%s:%d",s1, s2,num); break;
-	}	
-	
+	}
+
 	if((fd = open("server.log", O_CREAT| O_WRONLY | O_APPEND,0644)) >= 0) {
-		(void)write(fd,logbuffer,strlen(logbuffer)); 
-		(void)write(fd,"\n",1);      
+		(void)write(fd,logbuffer,strlen(logbuffer));
+		(void)write(fd,"\n",1);
 		(void)close(fd);
 	}
 	if(type == ERROR || type == SORRY) exit(3);
@@ -68,15 +68,15 @@ void web(int fd, int hit)
 	char * fstr;
 	static char buffer[BUFSIZE+1];
 
-	ret =read(fd,buffer,BUFSIZE); 
+	ret =read(fd,buffer,BUFSIZE);
 	if(ret == 0 || ret == -1) {
 		log(SORRY,"failed to read browser request","",fd);
 	}
-	if(ret > 0 && ret < BUFSIZE)	
-		buffer[ret]=0;	
+	if(ret > 0 && ret < BUFSIZE)
+		buffer[ret]=0;
 	else buffer[0]=0;
 
-	for(i=0;i<ret;i++)	
+	for(i=0;i<ret;i++)
 		if(buffer[i] == '\r' || buffer[i] == '\n')
 			buffer[i]='*';
 	log(LOG,"request",buffer,hit);
@@ -84,18 +84,18 @@ void web(int fd, int hit)
 	if( strncmp(buffer,"GET ",4) && strncmp(buffer,"get ",4) )
 		log(SORRY,"Only simple GET operation supported",buffer,fd);
 
-	for(i=4;i<BUFSIZE;i++) { 
-		if(buffer[i] == ' ') { 
+	for(i=4;i<BUFSIZE;i++) {
+		if(buffer[i] == ' ') {
 			buffer[i] = 0;
 			break;
 		}
 	}
 
-	for(j=0;j<i-1;j++) 	
+	for(j=0;j<i-1;j++)
 		if(buffer[j] == '.' && buffer[j+1] == '.')
 			log(SORRY,"Parent directory (..) path names not supported",buffer,fd);
 
-	if( !strncmp(&buffer[0],"GET /\0",6) || !strncmp(&buffer[0],"get /\0",6) ) 
+	if( !strncmp(&buffer[0],"GET /\0",6) || !strncmp(&buffer[0],"get /\0",6) )
 		(void)strcpy(buffer,"GET /index.html");
 
 	buflen=strlen(buffer);
@@ -109,7 +109,7 @@ void web(int fd, int hit)
 	}
 	if(fstr == 0) log(SORRY,"file extension type not supported",buffer,fd);
 
-	if(( file_fd = open(&buffer[5],O_RDONLY)) == -1) 
+	if(( file_fd = open(&buffer[5],O_RDONLY)) == -1)
 		log(SORRY, "failed to open file",&buffer[5],fd);
 
 	log(LOG,"SEND",&buffer[5],hit);
@@ -131,7 +131,7 @@ int main(int argc, char **argv)
 {
 	int i, port, pid, listenfd, socketfd, hit;
 	size_t length;
-	static struct sockaddr_in cli_addr; 
+	static struct sockaddr_in cli_addr;
 	static struct sockaddr_in serv_addr;
 
 	if( argc < 3  || argc > 3 || !strcmp(argv[1], "-?") ) {
@@ -152,18 +152,18 @@ int main(int argc, char **argv)
 		(void)printf("ERROR: Bad top directory %s, see server -?\n",argv[2]);
 		exit(3);
 	}
-	if(chdir(argv[2]) == -1){ 
+	if(chdir(argv[2]) == -1){
 		(void)printf("ERROR: Can't Change to directory %s\n",argv[2]);
 		exit(4);
 	}
 
 	if(fork() != 0)
-		return 0; 
-	(void)signal(SIGCLD, SIG_IGN); 
-	(void)signal(SIGHUP, SIG_IGN); 
+		return 0;
+	(void)signal(SIGCLD, SIG_IGN);
+	(void)signal(SIGHUP, SIG_IGN);
 	for(i=0;i<32;i++)
-		(void)close(i);	
-	(void)setpgrp();	
+		(void)close(i);
+	(void)setpgrp();
 
 	log(LOG,"http server starting",argv[1],getpid());
 
@@ -181,21 +181,20 @@ int main(int argc, char **argv)
 		log(ERROR,"system call","listen",0);
 
 	for(hit=1; ;hit++) {
-		length = sizeof(cli_addr);
-		if((socketfd = accept(listenfd, (struct sockaddr *)&cli_addr, &length)) < 0)
-			log(ERROR,"system call","accept",0);
+			length = sizeof(cli_addr);
+			if((socketfd = accept(listenfd, (struct sockaddr *)&cli_addr, &length)) < 0)
+				log(ERROR,"system call","accept",0);
 
-		if((pid = fork()) < 0) {
-			log(ERROR,"system call","fork",0);
-		}
-		else {
-			if(pid == 0) {
-				(void)close(listenfd);
-				web(socketfd,hit);
-			} else {
-				(void)close(socketfd);
+			if((pid = fork()) < 0) {
+				log(ERROR,"system call","fork",0);
 			}
-		}
+			else {
+				if(pid == 0) {
+					(void)close(listenfd);
+					web(socketfd,hit);
+				} else {
+					(void)close(socketfd);
+				}
+			}
 	}
 }
-
